7525517d3635221eaefc675d758d6c1bd37f8ea5
Trump <jasontrump68@gmail.com>
2023-11-10 20:55:34 +0800

    create podspec

---
+++ Example/TDTagsView/TDViewController.h
@@ -0,0 +1,13 @@
+//
+//  TDViewController.h
+//  TDTagsView
+//
+//  Created by TrumpRothschild on 11/10/2023.
+//  Copyright (c) 2023 TrumpRothschild. All rights reserved.
+//
+
+@import UIKit;
+
+@interface TDViewController : UIViewController
+
+@end
---
+++ Example/TDTagsView/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TDTagsView
+//
+//  Created by TrumpRothschild on 11/10/2023.
+//  Copyright (c) 2023 TrumpRothschild. All rights reserved.
+//
+
+@import UIKit;
+#import "TDAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TDAppDelegate class]));
+    }
+}
